0938d1c2f9f7aeedc22abcd4a5f55bdb01f6a450
farhanwaris78 <55206873+farhanwaris78@users.noreply.github.com>
2023-07-20 10:58:35 +0500

    Update KisskhProvider.kt

--- KisskhProvider/src/main/kotlin/com/farhanwaris78/KisskhProvider.kt
+++ KisskhProvider/src/main/kotlin/com/farhanwaris78/KisskhProvider.kt
@@ -1,5 +1,6 @@
 package com.farhanwaris78
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.mvvm.safeApiCall
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
